03200104b52edd7d2dc17d90c258942b41b0d577
tahislol21 <A100240976@my.uvm.edu.mx>
2025-05-08 07:36:04 -0600

    carga de archivos

---
+++ mi_script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const miBoton = document.getElementById('miBoton');
+
+    miBoton.addEventListener('click', function() {
+        alert('¡Hola! Gracias por interactuar.');
+        // Aquí puedes agregar más funcionalidades para el botón
+    });
+});
